1bb7fa4f720f6f5d44c3d9f7fa243a32138bb34b
Felix <me@felixbinder.net>
2024-04-22 19:54:29 +0000

    finetuning can now be turned off for specific models

--- scripts/sweep_full_study.py
+++ scripts/sweep_full_study.py
@@ -132,6 +132,7 @@ class StudyRunner:
             "--n_meta_val", type=int, help="Number of meta level completions for validation.", default=500
         )
         parser.add_argument("--skip_finetuning", action="store_true", help="Skip the finetuning step.", default=False)
+        parser.add_argument("--skip_finetuning_for_models", type=str, help="Comma-separated list of models to skip finetuning for.", default="")
         self.args = parser.parse_args()
 
     def run_command(self, command):
@@ -164,6 +165,7 @@ class StudyRunner:
             "prompt_configs",
             "inference_overrides",
             "finetuning_overrides",
+            "skip_finetuning_for_models"
         ]:
             setattr(
                 self.args, arg, getattr(self.args, arg).replace(", ", ",").split(",") if getattr(self.args, arg) else []
@@ -373,6 +375,9 @@ class StudyRunner:
         #### run finetuning ####
         finetuning_commands = []
         for model in self.args.model_configs:
+            if model in self.args.skip_finetuning_for_models:
+                print(f"Skipping finetuning for {model} because it is in --skip_finetuning_for_models.")
+                continue
             for ft_study in finetuning_study_names:
                 command = self.get_finetuning_command(
                     model, f"{self.args.study_name}/{ft_study}", "sweep", self.args.finetuning_overrides
