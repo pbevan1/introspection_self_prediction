42480e27945f27f35ca018951ee7dae01b3ccb22
James Chua <30519287+thejaminator@users.noreply.github.com>
2024-06-03 14:09:08 -0700

    Initial scripts for adding other evals finetuning utils, added arc for other evals (#65)
    * first implementation for finetuning
    
    * lint
    
    * wip
    
    * speed up by running on balanced data only
    
    * fix heatmap font
    
    * wip
    
    * add arc
    
    * add finetune for all
    
    * lint
    
    * remove unneeded

---
+++ other_evals/counterfactuals/datasets/load_arc.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+from string import ascii_uppercase
+
+from pydantic import BaseModel
+from slist import Slist
+from other_evals.counterfactuals.api_utils import read_jsonl_file_into_basemodel
+
+from other_evals.counterfactuals.datasets.base_example import DataExampleBase, MultipleChoiceAnswer
+
+
+class ArcChoices(BaseModel):
+    label: str
+    text: str
+
+
+class ArcQuestion(BaseModel):
+    stem: str
+    choices: list[ArcChoices]
+
+
+class ArcExample(DataExampleBase):
+    id: str
+    question: ArcQuestion
+    answerKey: str
+
+    def maybe_convert_label(self, label: str) -> MultipleChoiceAnswer:
+        if label.isnumeric():
+            # The arc dataset uses 1,2,3,4,5,6 ... lol
+            index = int(label) - 1
+            label = ascii_uppercase[index]  # type: ignore
+        return label  # type: ignore
+
+    def _get_question(self) -> str:
+        return self.question.stem
+
+    def _get_options(self) -> list[str]:
+        outputs = []
+        for option in self.question.choices:
+            # replace A)answer with (A) answer
+            text = option.text
+            outputs.append(text)
+        return outputs
+
+    @property
+    def _ground_truth(self) -> MultipleChoiceAnswer:
+        label = self.maybe_convert_label(self.answerKey)
+        return label
+
+
+def arc_easy_dev() -> list[ArcExample]:
+    dev_path = Path("other_evals/counterfactuals/datasets/arc_easy/ARC-Easy-Dev.jsonl")
+    return read_jsonl_file_into_basemodel(dev_path, ArcExample)
+
+
+def arc_easy_train() -> list[ArcExample]:
+    path = Path("other_evals/counterfactuals/datasets/arc_easy/ARC-Easy-Train.jsonl")
+    return read_jsonl_file_into_basemodel(path, ArcExample)
+
+
+def arc_easy_test() -> list[ArcExample]:
+    path = Path("other_evals/counterfactuals/datasets/arc_easy/ARC-Easy-Test.jsonl")
+    return read_jsonl_file_into_basemodel(path, ArcExample)
+
+
+def arc_challenge_dev() -> list[ArcExample]:
+    dev_path = Path("other_evals/counterfactuals/datasets/arc_challenge/ARC-Challenge-Dev.jsonl")
+    return read_jsonl_file_into_basemodel(dev_path, ArcExample)
+
+
+def arc_challenge_train() -> list[ArcExample]:
+    path = Path("other_evals/counterfactuals/datasets/arc_challenge/ARC-Challenge-Train.jsonl")
+    return read_jsonl_file_into_basemodel(path, ArcExample)
+
+
+def arc_challenge_test() -> list[ArcExample]:
+    path = Path("other_evals/counterfactuals/datasets/arc_challenge/ARC-Challenge-Test.jsonl")
+    return read_jsonl_file_into_basemodel(path, ArcExample)
+
+
+def arc_all() -> Slist[ArcExample]:
+    return Slist(arc_easy_train() + arc_easy_test() + arc_challenge_train() + arc_challenge_test())
