12e5ac10f6a4ac6bab3f102f9e2a5644b2ef9a03
James Chua <30519287+thejaminator@users.noreply.github.com>
2024-06-12 17:00:34 -0700

    Add model written evals yamls and jsons (#81)
    * initial wealth seeking property
    
    * add power_seeking, survival_instinct, fix wealth seeking
    
    * add jsonls
    
    * add yaml

--- other_evals/model_generated/load_wealth_seeking.py
+++ other_evals/model_generated/load_wealth_seeking.py
@@ -3,7 +3,7 @@ from typing import Literal
 from pydantic import BaseModel
 from slist import Slist
 
-from other_evals.counterfactuals.api_utils import read_jsonl_file_into_basemodel
+from other_evals.counterfactuals.api_utils import read_jsonl_file_into_basemodel, write_jsonl_file_from_basemodel
 
 
 class CleanedModelBehaviorEval(BaseModel):
@@ -44,5 +44,38 @@ def load_survival_instinct() -> Slist[CleanedModelBehaviorEval]:
     return load_behavior_eval(path)
 
 
+class RepoFormat(BaseModel):
+    string: str
+    target: str  # the answer matching behavior
+
+
+def to_repo_format(behavior_eval: CleanedModelBehaviorEval) -> RepoFormat:
+    return RepoFormat(string=behavior_eval.question, target=behavior_eval.answer_matching_behavior)
+
+
+def dump_wealth_seeking():
+    wealth_seeking = load_wealth_seeking()
+    results = wealth_seeking.map(to_repo_format).shuffle("42")
+    train, test = results.split_proportion(0.5)
+    write_jsonl_file_from_basemodel(basemodels=train, path=Path("evals/datasets/train_wealth_seeking.jsonl"))
+    write_jsonl_file_from_basemodel(basemodels=test, path=Path("evals/datasets/val_wealth_seeking.jsonl"))
+
+def dump_power_seeking():
+    power_seeking = load_power_seeking()
+    results = power_seeking.map(to_repo_format).shuffle("42")
+    train, test = results.split_proportion(0.5)
+    write_jsonl_file_from_basemodel(basemodels=train, path=Path("evals/datasets/train_power_seeking.jsonl"))
+    write_jsonl_file_from_basemodel(basemodels=test, path=Path("evals/datasets/val_power_seeking.jsonl"))
+
+def dump_survival_instinct():
+    survival_instinct = load_survival_instinct()
+    results = survival_instinct.map(to_repo_format).shuffle("42")
+    train, test = results.split_proportion(0.5)
+    write_jsonl_file_from_basemodel(basemodels=train, path=Path("evals/datasets/train_survival_instinct.jsonl"))
+    write_jsonl_file_from_basemodel(basemodels=test, path=Path("evals/datasets/val_survival_instinct.jsonl"))
+
 if __name__ == "__main__":
-    print(load_wealth_seeking()[0])
+    # print(load_wealth_seeking()[0])
+    # dump_wealth_seeking()
+    # dump_power_seeking()
+    dump_survival_instinct()
