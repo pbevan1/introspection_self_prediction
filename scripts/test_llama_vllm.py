f322b206e8e7d94a206ddc888d8ef98d4eabb247
James Chua <30519287+thejaminator@users.noreply.github.com>
2024-06-19 12:46:16 +0800

    fix lint with gemini and llama vllm scripts (#98)

--- scripts/test_llama_vllm.py
+++ scripts/test_llama_vllm.py
@@ -10,19 +10,20 @@ setup_environment()
 runpod_api_key = os.environ.get("RUNPOD_API_KEY")
 assert runpod_api_key is not None, "Please set RUNPOD_API_KEY in your environment variables"
 
-openai.api_base = f"https://api.runpod.ai/v2/vllm-xvb88yc7q0u9r4/openai/v1"
+openai.api_base = "https://api.runpod.ai/v2/vllm-xvb88yc7q0u9r4/openai/v1"
 openai.api_key = runpod_api_key
 
 
 async def test():
     result = await openai.ChatCompletion.acreate(  # type: ignore
-            model="meta-llama/Meta-Llama-3-8B",
-            messages=[{"role": "user", "content": "Why is google's gemini so janky"}],
-            max_tokens=1000,
+        model="meta-llama/Meta-Llama-3-8B",
+        messages=[{"role": "user", "content": "Why is google's gemini so janky"}],
+        max_tokens=1000,
     )
     choices = result["choices"]  # type: ignore
     for choice in choices:
         print(choice["message"]["content"])
+
 
 if __name__ == "__main__":
     import asyncio
