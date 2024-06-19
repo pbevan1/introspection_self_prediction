02acbe8113e355cafdf1bc372a4621eef1ba68f8
James Chua <chuajamessh@gmail.com>
2024-06-20 00:36:02 +0800

    add modal baseline

--- other_evals/counterfactuals/other_eval_csv_format.py
+++ other_evals/counterfactuals/other_eval_csv_format.py
@@ -3,9 +3,10 @@ from typing import Mapping
 from git import Sequence
 from pydantic import BaseModel
 import pandas as pd
-from slist import Group, Slist
+from slist import Slist
 
-from evals import response_property
+from evals.locations import EXP_DIR
+
 
 class NotebookColumns(BaseModel):
     # string response_object extracted_property_object extracted_property_meta response_property_at_merge
@@ -15,6 +16,7 @@ class NotebookColumns(BaseModel):
     extracted_property_meta: str
     response_property_at_merge: str
     compliance_meta: bool
+    mode_baseline: float  # calculated over object + meta
 
 
 class OtherEvalCSVFormat(BaseModel):
@@ -27,16 +29,15 @@ class OtherEvalCSVFormat(BaseModel):
     meta_predicted_correctly: bool
     eval_name: str
 
-
-
-    def to_notebook_columns(self) -> NotebookColumns:
+    def to_notebook_columns(self, mode_baseline: float) -> NotebookColumns:
         return NotebookColumns(
             string=self.object_history,
             response_object=self.object_parsed_result,
             extracted_property_object=self.object_parsed_result,
             extracted_property_meta=self.meta_parsed_result,
             response_property_at_merge=self.eval_name,
-            compliance_meta=True, # We only include compliant samples for now
+            compliance_meta=True,  # We only include compliant samples for now
+            mode_baseline=mode_baseline,
         )
 
 
@@ -54,22 +55,29 @@ def load_all_other_eval_csvs(results_path: str | Path) -> Sequence[OtherEvalCSVF
     for csv_path in all_files_ending_with_csv:
         data = pd.read_csv(csv_path)
         _json = data.to_dict(orient="records")
-        parsed.extend([OtherEvalCSVFormat(**row) for row in _json]) # type: ignore
+        parsed.extend([OtherEvalCSVFormat(**row) for row in _json])  # type: ignore
     print(f"Parsed {len(parsed)} other evals samples.")
     return parsed
 
 
+def mode_baseline(parsed: Sequence[OtherEvalCSVFormat]) -> float:
+    # mode_baseline is the percentage of samples that are predicted correctly
+    _parsed = Slist(parsed)
+    the_mode = _parsed.map(lambda x: x.meta_parsed_result).mode_or_raise()
+    # get number of modes
+    percent_modes = _parsed.map(lambda x: x.meta_parsed_result == the_mode).average_or_raise()
+    return percent_modes
 
 
 def to_notebook_groups(parsed: Sequence[OtherEvalCSVFormat]) -> Mapping[tuple[str, str], Sequence[NotebookColumns]]:
     # return object_model, meta_model -> list of OtherEvalCSVFormat
     parsed_ = Slist(parsed)
     # group by object_model and meta_model
-    grouped =  parsed_.group_by(lambda x: (x.object_model, x.meta_model))
-    mapped = grouped.map_on_group_values(lambda group_items: group_items.map(lambda x: x.to_notebook_columns()))
+    grouped = parsed_.group_by(lambda x: (x.object_model, x.meta_model))
+    mapped = grouped.map_on_group_values(
+        lambda group_items: group_items.map(lambda x: x.to_notebook_columns(mode_baseline=mode_baseline(group_items)))
+    )
     return mapped.to_dict()
-
-
 
 
 class FinetuneMessage(BaseModel):
@@ -84,3 +92,9 @@ class FinetuneConversation(BaseModel):
     @property
     def last_message_content(self) -> str:
         return self.messages[-1].content
+
+
+def test_this():
+    other_evals_samples = load_all_other_eval_csvs(EXP_DIR / "evaluation_suite" / "other_evals")
+    notebook_groups = to_notebook_groups(other_evals_samples)
+test_this()
